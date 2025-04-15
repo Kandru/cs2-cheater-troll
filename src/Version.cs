459bf43f39e381d2bd06deee6b130d3ddbdef6e7
Kalle <kalle@kandru.de>
2025-04-15 20:47:26 +0200

    bump version to 0.0.6

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace CheaterTroll
 {
     public partial class CheaterTroll : BasePlugin
     {
-        public override string ModuleVersion => "0.0.5";
+        public override string ModuleVersion => "0.0.6";
     }
 }
