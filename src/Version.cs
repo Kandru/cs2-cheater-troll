b1051c12cdc8aa0ba08adbefe0380042ef53a6bf
derkalle4 <kalle@kandru.de>
2025-12-25 10:53:53 +0100

    bump version to 25.12.1 to re-compile with latest CS#

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace CheaterTroll
 {
     public partial class CheaterTroll : BasePlugin
     {
-        public override string ModuleVersion => "0.0.11";
+        public override string ModuleVersion => "25.12.1";
     }
 }
