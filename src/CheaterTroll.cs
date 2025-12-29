5128feab4ad5896d985073ccd312289db8593ed2
derkalle4 <kalle@kandru.de>
2025-12-29 12:45:28 +0100

    fix: always try to load all current players

--- src/CheaterTroll.cs
+++ src/CheaterTroll.cs
@@ -28,11 +28,12 @@ namespace CheaterTroll
             RegisterEventHandler<EventPlayerDisconnect>(OnPlayerDisconnect);
             // register plugins
             InitializeModules(hotReload);
+            // check for players regardless if hot-reloaded or not (if loaded manually during round)
+            UpdatePlayerInfos();
+            EnableAllPlayerCheats();
             // check for hot reload
             if (hotReload)
             {
-                UpdatePlayerInfos();
-                EnableAllPlayerCheats();
                 Console.WriteLine(Localizer["core.hotreload"]);
             }
         }
