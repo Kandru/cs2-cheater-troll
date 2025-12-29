af88ae95052d04563febb670919da5b19d7b37c3
derkalle4 <kalle@kandru.de>
2025-12-30 00:46:21 +0100

    bump version to 25.12.11

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace CheaterTroll
 {
     public partial class CheaterTroll : BasePlugin
     {
-        public override string ModuleVersion => "25.12.10";
+        public override string ModuleVersion => "25.12.11";
     }
 }
