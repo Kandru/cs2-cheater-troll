6bcdd4d85e8a8fe3cd23efdb3fa1047c406151b3
derkalle4 <kalle@kandru.de>
2026-08-01 21:36:18 +0200

    bump version to 26.08.1 to update api changes

--- src/Version.cs
+++ src/Version.cs
@@ -4,6 +4,6 @@ namespace CheaterTroll
 {
     public partial class CheaterTroll : BasePlugin
     {
-        public override string ModuleVersion => "26.05.1";
+        public override string ModuleVersion => "26.08.1";
     }
 }
